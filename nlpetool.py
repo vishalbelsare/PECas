5155d07ae0ae45585dd529828aab4e8f9f6f5a5e
Adrian Buerger <adrian.buerger@hs-karlsruhe.de>
2014-11-27 16:31:30 +0100

    safety-commit before starting documentation with sphinx

--- nlpetool.py
+++ nlpetool.py
@@ -5,6 +5,130 @@ class NLPETool:
 
     '''Here to be the docstring for the class.'''
 
+    ##########################################################################
+    #### 1. Functions for checking validity and consistency of the inputs ####
+    ##########################################################################
+
+    def __check_variable_validity(self, var, varname, dtype, ddim):
+
+        '''
+        Check the validity of an input variables.
+        '''
+
+        # Check variable type
+
+        if type(var) is not dtype:
+            raise ValueError(\
+                '"{0}" needs to be of type "{1}".'.format(varname, dtype))
+
+        # Check if the variable contains a (column) vector
+
+        try:
+            if var.shape[1] is not ddim:
+                raise ValueError(\
+                    '"{0}" needs to be a (column) vector.'.format(varname))
+        except IndexError:
+
+            # If there is no second dimension given, the variable is also
+            # a column vector
+
+            pass
+    
+    # -----------------------------------------------------------------------#
+
+
+    def __check_variable_consistency(self, var1, ddim1, var2, ddim2):
+
+        '''
+        Check the consistency for two input variables.
+        '''
+
+        if ddim1 != ddim2:
+            raise ValueError('''
+The dimensions of the variables "{0}" and "{2}" do not match, since
+"{0}" has {1} entries, while "{2}"" has {4} entries.'''.format(\
+                var1, ddim1, var2, ddim2))
+
+
+    ##########################################################################
+    #### 2. Functions for getting and setting input and output variables #####
+    ##########################################################################
+
+    def set_x(self, x):
+
+        '''
+        Set the column vector for the parameters (type casadi.casadi_core.MX).
+        This function is automatically called at the initialization of the
+        object and only needed to change parameters after the initialization.
+        '''
+
+        self.__check_variable_validity(x, "x", ca.casadi_core.MX, 1)
+        self.__x = x
+
+
+    def get_x(self):
+
+        '''
+        Get the column vector for the parameters (type casadi.casadi_core.MX).
+        '''
+
+        return self.__x
+
+    # -----------------------------------------------------------------------#
+
+
+    def set_M(self, M):
+
+        '''
+        Set the column vector for the model (type casadi.casadi_core.MX).
+        This function is automatically called at the initialization of the
+        object and only needed to change parameters after the initialization.
+        '''
+
+        self.__check_variable_validity(M, "M", ca.casadi_core.MX, 1)
+        self.__M = M
+
+
+    def get_M(self):
+
+        '''
+        Get the column vector for the model (type casadi.casadi_core.MX).
+        '''
+
+        return self.__M
+
+    # -----------------------------------------------------------------------#
+
+
+    def set_sigma(self, sigma):
+
+        '''
+        Set the column vector for the standard deviation (type numpy.ndarray).
+        This function is automatically called at the initialization of the
+        object and only needed to change parameters after the initialization.
+        '''
+
+        self.__check_variable_validity(sigma, "sigma", np.ndarray, 1)
+        self.__sigma = sigma
+
+
+    def get_sigma(self):
+
+        '''
+        Get the column vector for the standard deviation (type numpy.ndarray).
+        '''
+
+        return self.__sigma
+
+    # -----------------------------------------------------------------------#
+
+
+
+
+    ##########################################################################
+    ################### Constructor for the class NLPETool ###################
+    ##########################################################################
+
     def __init__(\
         self, x, M, sigma, Y=None, xtrue=None, G=None, H=None, x0=None):
 
@@ -43,74 +167,42 @@ class NLPETool:
 
         '''
 
-
-        def check_variable_validity(var, varname, dtype, ddim):
-
-            # Check validity of input variables
-
-            # Check variable type
-
-            if type(var) is not dtype:
-                raise ValueError(\
-                    '"{0}" needs to be of type "{1}".'.format(varname, dtype))
-
-            # Check if the variable contains a (column) vector
-
-            try:
-                if var.shape[1] is not ddim:
-                    raise ValueError(\
-                        '"{0}" needs to be a (column) vector.'.format(varname))
-            except IndexError:
-
-                # If there is no second dimension given, the variable is also
-                # a column vector
-
-                pass
-        
-
-        def check_variable_consistency(var1, ddim1, var2, ddim2):
-
-            # Check consistency of input variables
-
-            if ddim1 != ddim2:
-                raise ValueError('''
-The dimensions of the variables "{0}" and "{2}" do not match, since
-"{0}" has {1} entries, while "{2}"" has {4} entries.'''.format(\
-                    var1, ddim1, var2, ddim2))
-
-
         ## Check variable validity ##
 
         # Necessary information
 
         # Parameters
 
-        check_variable_validity(x, "x", ca.casadi_core.MX, 1)
-        self.x = x
+        # self.__check_variable_validity(x, "x", ca.casadi_core.MX, 1)
+        # self.__x = x
+        self.set_x(x)
 
         # Model
 
-        check_variable_validity(M, "M", ca.casadi_core.MX, 1)
-        self.M = M
+        # self.__check_variable_validity(M, "M", ca.casadi_core.MX, 1)
+        # self.__M = M
+        self.set_M(M)
 
         # Standard deviations
 
-        check_variable_validity(sigma, "sigma", np.ndarray, 1)
-        self.sigma = sigma
+        # self.__check_variable_validity(sigma, "sigma", np.ndarray, 1)
+        # self.__sigma = sigma
+
+        self.set_sigma(sigma)
 
         # Mutually substitutable information
 
         # Measurements
 
         if Y is not None:
-            check_variable_validity(Y, "Y", np.ndarray, 1)
-            self.Y = Y
+            self.__check_variable_validity(Y, "Y", np.ndarray, 1)
+            self.__Y = Y
 
         # True value of x
 
         if xtrue is not None:
-            check_variable_validity(xtrue, "xtrue", np.ndarray, 1)
-            self.xtrue = xtrue
+            self.__check_variable_validity(xtrue, "xtrue", np.ndarray, 1)
+            self.__xtrue = xtrue
 
         # Assure that only one of the variables Y and xtrue is provided
 
@@ -130,20 +222,20 @@ value of x in "xtrue" to generate pseudo measurement data, not both.
         # Equality constrains
 
         if G is not None:
-            check_variable_validity(G, "G", ca.casadi_core.MX, 1)
-            self.G = G
+            self.__check_variable_validity(G, "G", ca.casadi_core.MX, 1)
+            self.__G = G
 
         # Inequality constrains
 
         if H is not None:
-            check_variable_validity(H, "H", ca.casadi_core.MX, 1)
-            self.H = H
+            self.__check_variable_validity(H, "H", ca.casadi_core.MX, 1)
+            self.__H = H
 
         # Initial guess
 
         if x0 is not None:
-            check_variable_validity(x0, "x0", np.ndarray, 1)
-            self.x0 = x0
+            self.__check_variable_validity(x0, "x0", np.ndarray, 1)
+            self.__x0 = x0
 
 
         ## Check variable consistency ##
@@ -151,29 +243,29 @@ value of x in "xtrue" to generate pseudo measurement data, not both.
         # Check whether M and sigma, and Y if already given, have the same
         # number of entries
 
-        check_variable_consistency("M", self.M.shape[0], \
-            "sigma", self.sigma.shape[0])
+        self.__check_variable_consistency("M", self.__M.shape[0], \
+            "sigma", self.__sigma.shape[0])
 
         if Y is not None:
-            check_variable_consistency("Y", self.Y.shape[0], \
-                "sigma", self.sigma.shape[0])
+            self.__check_variable_consistency("Y", self.__Y.shape[0], \
+                "sigma", self.__sigma.shape[0])
 
 
         ## Get problem dimension ##
 
         # Get the number of measurements N from M
 
-        self.N = self.M.shape[0]
+        self.__N = self.__M.shape[0]
 
         # Get the number of parameters d from x
 
-        self.d = self.x.shape[0]
+        self.__d = self.__x.shape[0]
 
         # If they exist, get the number of equality constraints from G
         if G is not None:
-            self.m = self.G.shape[0]
+            self.__m = self.__G.shape[0]
         else:
-            self.m = 0
+            self.__m = 0
 
 
     def generate_pseudo_measurement_data(self):
@@ -184,15 +276,15 @@ value of x in "xtrue" to generate pseudo measurement data, not both.
 
         '''
 
-        self.Mx = ca.MXFunction(ca.nlpIn(x=self.x), ca.nlpOut(f=self.M))
-        self.Mx.setOption("name", "Model function Mx")
-        self.Mx.init()
+        self.__Mx = ca.MXFunction(ca.nlpIn(x=self.__x), ca.nlpOut(f=self.__M))
+        self.__Mx.setOption("name", "Model function Mx")
+        self.__Mx.init()
 
-        self.Mx.setInput(self.xtrue, "x")
-        self.Mx.evaluate()
+        self.__Mx.setInput(self.__xtrue, "x")
+        self.__Mx.evaluate()
 
-        self.Y = self.Mx.getOutput("f") + \
-            np.random.normal(0, self.sigma, self.N)
+        self.__Y = self.__Mx.getOutput("f") + \
+            np.random.normal(0, self.__sigma, self.__N)
 
 
     def run_parameter_estimation(self):
@@ -208,15 +300,15 @@ value of x in "xtrue" to generate pseudo measurement data, not both.
 
         # Check whether M, sigma and Y have the same number of entries
 
-        # check_variable_consistency("M", self.M.shape[0], \
-        #     "sigma", self.sigma.shape[0])
+        # self.__check_variable_consistency("M", self.__M.shape[0], \
+        #     "sigma", self.__sigma.shape[0])
 
         # # With this, also check if measurements data exists; if not,
         # # generate it
 
         # try:
-        #     check_variable_consistency("Y", self.Y.shape[0], \
-        #         "sigma", self.sigma.shape[0])
+        #     self.__check_variable_consistency("Y", self.__Y.shape[0], \
+        #         "sigma", self.__sigma.shape[0])
         # except NameError:
         #     self.generate_pseudo_measurement_data()            
 
