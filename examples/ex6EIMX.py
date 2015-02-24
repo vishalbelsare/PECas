a35d11af15876e24a518964b3f9d9a6959f67412
jeslago <jesuslagogarcia@gmail.com>
2015-02-24 18:23:34 +0100

    Example 6 using a first order Euler integrator and MX symbolic expressions. Used for comparision of processing speed and RAM memory allocation of the other 5 versions of example 6 (Euler vs. Runge–Kutta order 4 vs. Casadi Single Shooring & SX vs. MX)

---
+++ examples/ex6EIMX.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+# ex6.py: Example 6 shows how to estimate the parameters of a general system
+# described by an ODE with no analitical solution using the Euler's method.
+# Furthermore, it illustrates a parametric identification where two different
+# kind of measurements (angle and angular speed) are used, which have different
+# standar deviation.
+
+import pecas as pc
+import pylab as pl
+import casadi as ca
+
+def mainEx6EIMX():
+    data = pl.loadtxt('ex6data.txt')
+    
+    t = data[:, 0]
+    phim = data[:, 1]
+    wm = data[:, 2]
+    N = t.size
+    
+    m = 1  # Ball mass in Kg
+    L = 3  # Rod length in meters
+    g = 9.81  # Gravity constant in m/s^2
+    psi = pl.pi/2  # Handler (actuation) angle in radians
+    
+    dT = ca.MX.sym("dT", 1)
+    phip = ca.MX.sym("phip", 1)
+    wp = ca.MX.sym("wp", 1)
+    
+    # Theta = [Phi0,W0,K]
+    Theta = ca.MX.sym("Theta", 3)
+    
+    # State x = [phi,w]
+    
+    # simstep
+    phis = phip + wp*dT
+    ws = wp + (Theta[2]/(m*(L**2))*(psi-phip) - g/L * pl.sin(phip))*dT
+    
+    fphiw = ca.MXFunction([Theta, phip, wp, dT], [phis, ws])
+    fphiw.setOption("name", "fphiw")
+    fphiw.init()
+    
+    
+    # simsloop
+    
+    Model = ca.MX.zeros(2*N, 1)    
+    Model[0] = Theta[0]
+    Model[t.size] = Theta[1]
+    
+    for k_phi in xrange(1, N):
+        k_w = t.size+k_phi
+        simfun = fphiw([Theta, Model[k_phi-1], Model[k_w-1],
+                        t[k_phi] - t[k_phi-1]])
+        Model[k_phi] = simfun[0]
+        Model[k_w] = simfun[1]
+    
+    sigmaphi = pl.ones(t.size)*pl.std(phim, ddof=1)
+    sigmaw = pl.ones(t.size)*pl.std(wm, ddof=1)
+    sigma = pl.concatenate([sigmaphi, sigmaw])
+    xinit = pl.ones(3)
+    
+    pep = pc.PECasLSq(Theta, Model, sigma, Y=pl.concatenate([phim, wm]),
+                      xinit=xinit)
+    pep.run_parameter_estimation()
+    
+    print pep.get_xhat()
+    
+    pep.compute_covariance_matrix()
+    pep.print_results()
+    return
+    
+if __name__ == "__main__":
+    mainEx6EIMX()
