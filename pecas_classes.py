721c0d68d5732443ebccdccd50f2e02d6442e9b3
Adrian Buerger <adrian.buerger@hs-karlsruhe.de>
2015-03-02 16:39:56 +0100

    safety-commit before removing single-shooting again

--- pecas_classes.py
+++ pecas_classes.py
@@ -3,70 +3,39 @@ import casadi.tools as cat
 
 class BasicProblem(object):
 
-
-    __v_struct = cat.struct_symMX(["p", "x", "u", "z", "t"])
-    __fg_struct = cat.struct_symMX(["f", "g"])
-    __meas = cat.struct_symMX(["tg", "meas"])
-
-
-    def __check_for_valid_input(self, inp):
-
-        if inp is not None:
-
-            return inp
-
-        else:
-
-            raise ValueError('''
-You have to specify a value for setting up this class.
-''')
-
-
     def __init__(self, \
-                 p = None, pmin = None, pmax = None, \
-                 t = None, \
-                 f = None, g = None, \
-                 tg = None, meas = None):
+                 t = ca.SX.sym("t", 1), u = ca.SX.sym("u", 0), \
+                 x = ca.SX.sym("x", 0), z = ca.SX.sym("z", 0), \
+                 p = None, \
+                 y = None, \
+                 f = ca.SX.sym("f", 0), g = ca.SX.sym("g", 0)):
 
-        self.v = self.__v_struct()
-        self.vmin = self.__v_struct()
-        self.vmax = self.__v_struct()
+        self.v = cat.struct_MX([
+                (
+                    cat.entry("t", expr = t),
+                    cat.entry("u", expr = u),
+                    cat.entry("x", expr = x),
+                    cat.entry("z", expr = z),
+                    cat.entry("p", expr = p)
+                )
+            ])
 
-        self.fg = self.__fg_struct()
-
-        self.meas = self.__meas()
-
-        self.v["p"] = self.__check_for_valid_input(p)
-        self.vmin["p"] = self.__check_for_valid_input(pmin)
-        self.vmax["p"] = self.__check_for_valid_input(pmax)
-
-        self.v["t"] = self.__check_for_valid_input(t) # optional!
-
-        self.fg["f"] = self.__check_for_valid_input(f)
-        self.fg["g"] = self.__check_for_valid_input(g) # optional!
-
-        self.meas["tg"] = self.__check_for_valid_input(tg)
-        self.meas["meas"] = self.__check_for_valid_input(meas)
+        self.fcn = cat.struct_MX([
+                (
+                    cat.entry("y", expr = y),
+                    cat.entry("f", expr = f),
+                    cat.entry("g", expr = g)
+                )
+            ])
 
 
 class ODEProblem(BasicProblem):
 
-
-    def __check_for_valid_input(self, inp):
-
-        return inp
-        # return super(ODEProblem, self).__check_for_valid_input(inp)
-
-
     def __init__(self, \
-                 p = None, pmax = None, pmin = None, \
-                 x = None, xmin = None, xmax = None, \
-                 t = None, \
-                 f = None, g = None, \
-                 tg = None, meas = None):
+                 t = ca.SX.sym("t", 1), u = ca.SX.sym("u", 0), \
+                 x = None, z = ca.SX.sym("z", 0), \
+                 p = None, \
+                 y = None, \
+                 f = None, g = ca.SX.sym("g", 0)):
 
-        super(ODEProblem, self).__init__(p, pmax, pmin, f, g)
-
-        self.v["x"] = self.__check_for_valid_input(x)
-        self.vmin["x"] = self.__check_for_valid_input(xmin)
-        self.vmax["x"] = self.__check_for_valid_input(xmax)
+        super(ODEProblem, self).__init__(t = t, u = u, x = x, z = z, p = p, y = y, f = f, g = g)
--- pecas_solvers.py
+++ pecas_solvers.py
@@ -1,10 +1,69 @@
-def evaluate():
+import casadi as ca
+import casadi.tools as cat
+
+class BPEval:
+
+    def __init__(self, bp, N):
+
+        self.Y = []
+        self.G = []
+
+        self.V = cat.struct_symMX([
+                (
+                    cat.entry("T", repeat = [N], shape = bp.v["t"].shape),
+                    cat.entry("U", repeat = [N], shape = bp.v["u"].shape),
+                    cat.entry("P", shape = bp.v["p"].shape),
+                    cat.entry("W", repeat = [N], shape = bp.fcn["g"].shape)
+                )
+            ])
+
+        yfcn = ca.SXFunction([bp.v["t"], bp.v["u"], bp.v["p"]], [bp.fcn["y"]])
+        yfcn.init()
+
+        gfcn = ca.SXFunction([bp.v["t"], bp.v["u"], bp.v["p"]], [bp.fcn["g"]])
+        gfcn.init()
+
+        for k in range(N):
+
+            self.Y.append(yfcn.call([self.V["T"][k], self.V["U"][k], \
+                self.V["P"]])[0])
+            self.G.append(gfcn.call([self.V["T"][k], self.V["U"][k], \
+                self.V["P"]])[0])
+
+        self.Y = ca.vertcat(self.Y)
+        self.G = ca.vertcat(self.G)
 
 
-def explode():
+class ODESolSingleShooting:
 
+    def __init__(self, op, N, x0):
 
-def implode():
+        self.Y = []
+        self.G = []
 
+        self.V = cat.struct_MX([
+                (
+                    cat.entry("T", repeat = [N], shape = op.v["t"].shape),
+                    cat.entry("U", repeat = [N], shape = op.v["u"].shape),
+                    cat.entry("X", repeat = [N], shape = op.v["x"].shape),
+                    cat.entry("P", shape = op.v["p"].shape),
+                    cat.entry("W", repeat = [N], shape = op.fcn["g"].shape)
+                )
+            ])
 
-def implcollocation():
+        ffcn = ca.SXFunction([op.v["t"], op.v["u"], op.v["x"], op.v["p"]],
+            [op.fcn["f"]])
+        ffcn.init()
+
+        # self.V["X", 0, :] = x0
+
+        for k in range(N-1):
+
+            [K1] = ffcn.call([self.V["T", k], self.V["U", k, :], self.V["X", k, :], self.V["P", :]])
+            [K2] = ffcn.call([self.V["T", k], self.V["U", k, :], self.V["X", k, :] + 0.5 * K1 * (self.V["T", k+1] - self.V["T", k]), self.V["P", :]])
+            [K3] = ffcn.call([self.V["T", k], self.V["U", k, :], self.V["X", k, :] + 0.5 * K2 * (self.V["T", k+1] - self.V["T", k]), self.V["P", :]])
+            [K4] = ffcn.call([self.V["T", k], self.V["U", k, :], self.V["X", k, :] + K3 * (self.V["T", k+1] - self.V["T", k]), self.V["P", :]])
+
+            self.V["X", k+1, :] = (1.0 / 6.0) * (K1 + 2 * K2 + 2 * K3 + K4) * (self.V["T", k+1] - self.V["T", k])
+
+            # self.V["X", k + 1, :] = ca.integratorOut(integrator(ca.integratorIn([self.V["T", k], self.V["U", k, :], self.V["X", k, :], self.V["P", :]])))
