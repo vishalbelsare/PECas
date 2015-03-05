b7cdfa735f1d031a2ee9ae28021091aeb8a9d758
Adrian Buerger <adrian.buerger@hs-karlsruhe.de>
2015-03-05 14:11:43 +0100

    adjusted systems ot be more failsave

--- pecas/systems.py
+++ pecas/systems.py
@@ -7,18 +7,44 @@ import casadi.tools as cat
 class BasicSystem(object):
 
     def __init__(self, \
-                 t = ca.SX.sym("t", 1), u = ca.SX.sym("u", 0), \
-                 x = ca.SX.sym("x", 0), z = ca.SX.sym("z", 0), \
+                 t = ca.SX.sym("t", 1), \
+                 u = ca.SX.sym("u", 0), \
                  p = None, \
                  y = None, \
-                 f = ca.SX.sym("f", 0), g = ca.SX.sym("g", 0)):
+                 g = ca.SX.sym("g", 0)):
+
+        self.v = cat.struct_MX([
+                (
+                    cat.entry("t", expr = t),
+                    cat.entry("u", expr = u),
+                    cat.entry("p", expr = p)
+                )
+            ])
+
+        self.fcn = cat.struct_MX([
+                (
+                    cat.entry("y", expr = y),
+                    cat.entry("g", expr = g)
+                )
+            ])
+
+
+class ExplODE(BasicSystem):
+
+    def __init__(self, \
+                 t = ca.SX.sym("t", 1),
+                 u = ca.SX.sym("u", 0), \
+                 x = None, \
+                 p = None, \
+                 y = None, \
+                 f = None, \
+                 g = ca.SX.sym("g", 0)):
 
         self.v = cat.struct_MX([
                 (
                     cat.entry("t", expr = t),
                     cat.entry("u", expr = u),
                     cat.entry("x", expr = x),
-                    cat.entry("z", expr = z),
                     cat.entry("p", expr = p)
                 )
             ])
@@ -30,15 +56,3 @@ class BasicSystem(object):
                     cat.entry("g", expr = g)
                 )
             ])
-
-
-class ExplODE(BasicSystem):
-
-    def __init__(self, \
-                 t = ca.SX.sym("t", 1), u = ca.SX.sym("u", 0), \
-                 x = None, z = ca.SX.sym("z", 0), \
-                 p = None, \
-                 y = None, \
-                 f = None, g = ca.SX.sym("g", 0)):
-
-        super(ExplODE, self).__init__(t = t, u = u, x = x, p = p, y = y, f = f)
