5a32f94f72358cfc4361eb65a9cf084596e2d2c7
Adrian Buerger <adrian.buerger@hs-karlsruhe.de>
2015-03-09 11:54:08 +0100

    added test for use of multiple controls

--- test/test_ode_setup.py
+++ test/test_ode_setup.py
@@ -15,8 +15,10 @@ class BaseClassODESetupTest(object):
 
         # Test valid input dimensions for timegrid
 
-        pecas.setupmethods.ODEsetup(system = self.odesys, timegrid = self.timegrid)
-        pecas.setupmethods.ODEsetup(system = self.odesys, timegrid = self.timegrid.T)
+        pecas.setupmethods.ODEsetup(system = self.odesys, \
+            timegrid = self.timegrid)
+        pecas.setupmethods.ODEsetup(system = self.odesys, timegrid = \
+            self.timegrid.T)
 
 
     def test_systems_argument(self):
@@ -24,8 +26,8 @@ class BaseClassODESetupTest(object):
         # Support an invalid systems-type
 
         bssys = pecas.systems.BasicSystem(p = self.p, y = self.p)
-        self.assertRaises(TypeError, pecas.setupmethods.ODEsetup, system = bssys, \
-            timegrid = self.timegrid)
+        self.assertRaises(TypeError, pecas.setupmethods.ODEsetup, \
+            system = bssys, timegrid = self.timegrid)
 
 
     def test_parameter_bounds_initials_inputs(self):
@@ -136,6 +138,8 @@ class TestLotkaVolterra(unittest.TestCase, BaseClassODESetupTest):
 
     def setUp(self):
 
+        # System
+
         self.x = ca.SX.sym("x", 2)
         self.p = ca.SX.sym("p", 4)
         self.u = ca.SX.sym("u", 0)
@@ -149,10 +153,12 @@ class TestLotkaVolterra(unittest.TestCase, BaseClassODESetupTest):
         self.odesys = pecas.systems.ExplODE(x = self.x, u = self.u, \
             p = self.p, f = self.f, y = self.y)
 
+        # Inputs
+
         self.timegrid = pl.linspace(0, 10, 11)
 
-        self.invalidpargs = [[0, 1, 2], [[2, 3], [2, 3]], pl.asarray([1, 2, 3]), \
-            pl.asarray([[2, 3], [2, 3]])]
+        self.invalidpargs = [[0, 1, 2], [[2, 3], [2, 3]], \
+            pl.asarray([1, 2, 3]), pl.asarray([[2, 3], [2, 3]])]
         self.validpargs = [None, [0, 1, 2, 3], pl.asarray([1, 2, 3, 4]), \
             pl.asarray([1, 2, 3, 4]).T, pl.asarray([[2], [3], [2], [3]])]
 
@@ -178,6 +184,100 @@ class TestLotkaVolterra(unittest.TestCase, BaseClassODESetupTest):
         # --> How to check for this?
 
 
+class Test1DVehicle(unittest.TestCase, BaseClassODESetupTest):
+
+    def setUp(self):
+
+        # System
+
+        self.x = ca.SX.sym("x", 1)
+        self.p = ca.SX.sym("p", 3)
+        self.u = ca.SX.sym("u", 1)
+
+        self.f = self.p[0] * self.u - self.p[1] - self.p[2] * self.x
+
+        self.y = self.x
+
+        self.odesys = pecas.systems.ExplODE(x = self.x, u = self.u, \
+            p = self.p, f = self.f, y = self.y)
+
+        # Inputs
+
+        self.timegrid = pl.linspace(0, 6, 100)
+
+        self.invalidpargs = [[0, 1], [[2, 3], [2, 3]], \
+            pl.asarray([1, 2, 3, 4]), pl.asarray([[2, 3], [2, 3]])]
+        self.validpargs = [None, [0, 1, 2], pl.asarray([3, 4, 5]), \
+            pl.asarray([1, 2, 1]).T, pl.asarray([[2], [3], [4]])]
+
+        self.invalidxargs = [pl.ones((self.x.size() - 1, self.timegrid.size)), \
+            pl.ones((self.timegrid.size - 1, self.x.size()))]
+        self.validxargs = [None, pl.ones((self.x.size(), self.timegrid.size)), \
+            pl.ones((self.timegrid.size, self.x.size()))]
+
+        self.invalidxbvpargs = [[3, 2], pl.ones(3), \
+            pl.ones((1, 3)), pl.ones((3, 1))]
+        self.validxbvpargs = [None, [1], 1, pl.ones(1), pl.ones((1, 1))]
+
+        self.invaliduargs = [pl.ones((self.u.size(), self.timegrid.size)), \
+            pl.ones((self.timegrid.size, self.u.size()))]
+        self.validuargs = [None, pl.ones((self.u.size(), \
+            self.timegrid.size - 1)), \
+            pl.ones((self.timegrid.size - 1, self.u.size()))]
+
+
+class Test2DVehicle(unittest.TestCase, BaseClassODESetupTest):
+
+    def setUp(self):
+
+        # System
+
+        self.x = ca.SX.sym("x", 6)
+        self.p = ca.SX.sym("p", 6)
+        self.u = ca.SX.sym("u", 2)
+
+        self.f = ca.vertcat( \
+            [self.x[3] * pl.cos(self.x[2] + self.p[0] * self.x[4]),
+            self.x[3] * pl.sin(self.x[2] + self.p[0] * self.x[4]),
+            self.x[3] * self.x[4] * self.p[1],
+            self.p[2] * self.x[5] - self.p[3] * self.x[5] * self.x[3] \
+                - self.p[4] * self.x[3]**2 - self.p[5] \
+                - (self.x[3]  * self.x[5])**2 * self.p[1] * self.p[0],
+            self.u[0],
+            self.u[1]
+            ])
+
+        self.y = self.x[:4]
+
+        self.odesys = pecas.systems.ExplODE(x = self.x, u = self.u, \
+            p = self.p, f = self.f, y = self.y)
+
+        # Inputs
+
+        self.timegrid = pl.linspace(0, 6, 100)
+
+        self.invalidpargs = [[0, 1], [[2, 3], [2, 3]], \
+            pl.asarray([1, 2, 3, 4, 5]), pl.asarray([[2, 3], [2, 3]])]
+        self.validpargs = [None, pl.asarray([3, 4, 5, 5, 6, 7]), \
+            pl.asarray([1, 2, 1, 5, 6, 7]).T, \
+            pl.asarray([[2], [3], [4], [5], [6], [7]]), \
+            [1, 2, 3, 4, 5, 6]]
+
+        self.invalidxargs = [pl.ones((self.x.size() - 1, self.timegrid.size)), \
+            pl.ones((self.timegrid.size - 1, self.x.size()))]
+        self.validxargs = [None, pl.ones((self.x.size(), self.timegrid.size)), \
+            pl.ones((self.timegrid.size, self.x.size()))]
+
+        self.invalidxbvpargs = [[3, 2, 5], pl.ones(5), \
+            pl.ones((1, 5)), pl.ones((4, 1))]
+        self.validxbvpargs = [None, [1] * 6, pl.ones(6), pl.ones((1, 6)), \
+            pl.ones((6, 1))]
+
+        self.invaliduargs = [pl.ones((self.u.size(), self.timegrid.size)), \
+            pl.ones((self.timegrid.size, self.u.size()))]
+        self.validuargs = [None, pl.ones((self.u.size(), \
+            self.timegrid.size - 1)), \
+            pl.ones((self.timegrid.size - 1, self.u.size()))]
 
 # if __name__ == '__main__':
 #     unittest.main()
