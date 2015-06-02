54e21e28d9168610e75f1b293c8f47ac70e6bfa1
jeslago <jesuslagogarcia@gmail.com>
2015-06-02 13:19:59 +0200

    Including equation noise in output equation to allow noise in the inputs

--- pecas/setups.py
+++ pecas/setups.py
@@ -417,7 +417,7 @@ class CollocationBaseClass(SetupsBaseClass):
         self.phiN = []
 
         yfcn = ca.MXFunction([system.vars["t"], system.vars["x"], \
-            system.vars["p"], system.vars["u"]], [system.fcn["y"]])
+            system.vars["p"], system.vars["u"],system.vars["w"]], [system.fcn["y"]])
         yfcn.setOption("name", "yfcn")
         yfcn.init()
 
@@ -426,10 +426,10 @@ class CollocationBaseClass(SetupsBaseClass):
             # DEPENDECY ON U NOT POSSIBLE AT THIS POINT! len(U) = N, not N + 1!
             # self.phiN.append(yfcn.call([self.timegrid[k], self.Vars["U", k, 0], \
             self.phiN.append(yfcn.call([self.timegrid[k], self.Vars["X", k, 0], \
-                self.Vars["P"], self.u[:, k]])[0])
+                self.Vars["P"], self.u[:, k], self.Vars["W", k, 0]])[0])
 
         self.phiN.append(yfcn.call([self.timegrid[k], self.Vars["XF"], \
-            self.Vars["P"], self.u[:, -1]])[0])
+            self.Vars["P"], self.u[:, -1],self.Vars["W", -1, 0]])[0])
 
         self.phiN = ca.vertcat(self.phiN)
 
